60a4873e3eb0785cfeadec3fccf063185b96bde4
alestor123 <alestoraldous@gmail.com>
2021-06-03 19:07:48 +0530

    adding buttons

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -1,3 +1,10 @@
 var audios = ['bleep','dolphin','quack','distortion'],
 btns = document.getElementById('btns');
 audio = document.getElementById('audio');
+audios.forEach(sound => {
+var btn = document.createElement('button')
+btn.id = 'button'
+btn.innerHTML = sound.charAt(0).toUpperCase() + sound.slice(1)
+btn.setAttribute('onclick',`playEffect('${sound}')`)
+btns.appendChild(btn)
+})
