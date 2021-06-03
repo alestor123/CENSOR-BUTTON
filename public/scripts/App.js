66bf76be7570dd4f4f69bc03a1ac95e8a1648dc3
alestor123 <alestoraldous@gmail.com>
2021-06-03 19:10:00 +0530

    added linting

--- public/scripts/App.js
+++ public/scripts/App.js
@@ -1,14 +1,15 @@
-var audios = ['bleep','dolphin','quack','distortion'],
-btns = document.getElementById('btns');
-audio = document.getElementById('audio');
+const audios = ['bleep', 'dolphin', 'quack', 'distortion']
+const btns = document.getElementById('btns')
+audio = document.getElementById('audio')
 audios.forEach(sound => {
-var btn = document.createElement('button')
-btn.id = 'button'
-btn.innerHTML = sound.charAt(0).toUpperCase() + sound.slice(1)
-btn.setAttribute('onclick',`playEffect('${sound}')`)
-btns.appendChild(btn)
+  const btn = document.createElement('button')
+  btn.id = 'button'
+  btn.innerHTML = sound.charAt(0).toUpperCase() + sound.slice(1)
+  btn.setAttribute('onclick', `playEffect('${sound}')`)
+  btns.appendChild(btn)
 })
-function playEffect(name) {
-audio.src = `./public/assets/audio/${name}.mp3`
-audio.play()
+
+function playEffect (name) {
+  audio.src = `./public/assets/audio/${name}.mp3`
+  audio.play()
 }
